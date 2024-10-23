f6d0a2ed0419466b44c04c82bfd4571e7b53ebe3
F0ximu5 <jfovet@gmail.com>
2024-10-23 09:17:55 +0200

    List

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -4,9 +4,18 @@ namespace App\Http\Controllers;
 
 use App\Models\Movie;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class MovieController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Inertia::render('Movie/Index');
+    }
+
     public function search(Request $request)
     {
         $search_result = Movie::search($request->keyword)->paginateRaw(2); // TODO: Testing, use real pagination value for load-more behavior
