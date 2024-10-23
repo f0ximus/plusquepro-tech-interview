978b73b36c12ce2f96183b67cc60dce5166da16c
F0ximu5 <jfovet@gmail.com>
2024-10-23 20:16:54 +0200

    Hotfix: Search breaks when no search term

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -57,11 +57,17 @@ class MovieController extends Controller
 
     public function search(Request $request)
     {
-        $search_result = Movie::search($request->keyword, function($meiliSearch, string $query, array $options) {
-            $options['attributesToHighlight'] = ['original_title', 'overview'];
-            return $meiliSearch->search($query, $options);
-        })->paginateRaw(10);
+        if(!$request->keyword) {
+            $search_result = Movie::search();
+        } else {
+            $search_result = Movie::search($request->keyword, function($meiliSearch, string $query, array $options) {
+                $options['attributesToHighlight'] = ['original_title', 'overview'];
+                return $meiliSearch->search($query, $options);
+            });
+        }
 
-        return response()->json(['result' => $search_result]);
+        return response()->json([
+            'result' => $search_result->paginateRaw(10)
+        ]);
     }
 }
